14def9ac3b666c72c095db2df9e4a327eb7e2282
Marilyn Marquez <marilynmarquez@MMarquez.local>
2022-11-10 13:40:53 -0600

    added post method

--- mysite/pdf/views.py
+++ mysite/pdf/views.py
@@ -4,4 +4,17 @@ from .models import Profile
 # Create your views here.
 
 def accept(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        phone = request.POST.get("phone", "")
+        summary = request.POST.get("summary", "")
+        degree = request.POST.get("degree", "")
+        school = request.POST.get("school", "")
+        university = request.POST.get("university", "")
+        previous_role = request.POST.get("previous_role", "")
+        skills = request.POST.get("skills", "")
+
+        profile = Profile(name="name", email="email", phone="phone", summary="summary", degree="degree",school="school", university="university", previous_role="previous_role", skills="skills")
+        profile.save()
     return render (request, 'pdf/accept.html')
